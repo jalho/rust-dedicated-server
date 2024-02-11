b59a985845cd89e3827af8fba3b401680afeddf9
jalho <alho.jere@gmail.com>
2024-02-11 14:12:03 +0200

    Wrap some RDS params in quotes

--- scripts/start-rds.sh
+++ scripts/start-rds.sh
@@ -8,4 +8,4 @@ set -xe
 
 source /home/rust/carbon.sh
 
-/home/rust/RustDedicated -batchmode +server.identity $RDS_INSTANCE_ID +rcon.port $RCON_PORT +rcon.web 1 +rcon.password $RCON_PASSWORD
+/home/rust/RustDedicated -batchmode +server.identity "$RDS_INSTANCE_ID" +rcon.port $RCON_PORT +rcon.web 1 +rcon.password "$RCON_PASSWORD"
