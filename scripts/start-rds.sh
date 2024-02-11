55089cd3e6b3700e6d9dc99d93f4648621cb609c
jalho <alho.jere@gmail.com>
2024-02-11 14:23:49 +0200

    Specify logfile

--- scripts/start-rds.sh
+++ scripts/start-rds.sh
@@ -3,9 +3,10 @@
 RCON_PORT=28016
 RCON_PASSWORD="Your_Rcon_Password"
 RDS_INSTANCE_ID="instance0"
+RDS_LOGFILE="/home/rust/rds_$(date +%s).log"
 
 set -xe
 
 source /home/rust/carbon.sh
 
-/home/rust/RustDedicated -batchmode +server.identity "$RDS_INSTANCE_ID" +rcon.port $RCON_PORT +rcon.web 1 +rcon.password "$RCON_PASSWORD"
+/home/rust/RustDedicated -batchmode -logfile "$RDS_LOGFILE" +server.identity "$RDS_INSTANCE_ID" +rcon.port 28016 +rcon.web 1 +rcon.password "$RCON_PASSWORD"
