6f81f5cc309ac8aab1bd04d3626f3964721f6f36
jalho <alho.jere@gmail.com>
2024-02-18 00:47:23 +0200

    Use logfile

--- run-with-carbon.sh
+++ run-with-carbon.sh
@@ -3,4 +3,4 @@
 cd /home/rust/
 /home/rust/steamcmd/steamcmd.sh +force_install_dir /home/rust/ +login anonymous +app_update 258550 validate +quit
 source /home/rust/carbon.sh
-/home/rust/RustDedicated -batchmode +server.identity instance0 +rcon.port 28016 +rcon.web 1 +rcon.password Your_Rcon_Password
+/home/rust/RustDedicated -batchmode -logfile rds.log +server.identity instance0 +rcon.port 28016 +rcon.web 1 +rcon.password Your_Rcon_Password
