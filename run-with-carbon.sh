82da6d94e1df10701ca91060d4dd8c8640004f79
jalho <alho.jere@gmail.com>
2024-02-18 01:29:25 +0200

    Source carbon/tools/environment.sh instead of carbon.sh

--- run-with-carbon.sh
+++ run-with-carbon.sh
@@ -2,5 +2,5 @@
 
 cd /home/rust/
 /home/rust/steamcmd/steamcmd.sh +force_install_dir /home/rust/ +login anonymous +app_update 258550 validate +quit
-source /home/rust/carbon.sh
+source /home/rust/carbon/tools/environment.sh
 /home/rust/RustDedicated -batchmode -logfile rds.log +server.identity instance0 +rcon.port 28016 +rcon.web 1 +rcon.password Your_Rcon_Password
