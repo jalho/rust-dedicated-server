dad091dd15098686058eea7e76ba1857b51c1bef
jalho <alho.jere@gmail.com>
2024-02-25 00:54:35 +0200

    set -e

--- run-with-carbon.sh
+++ run-with-carbon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cd /home/rust/
 /home/rust/steamcmd/steamcmd.sh +force_install_dir /home/rust/ +login anonymous +app_update 258550 validate +quit
 source /home/rust/carbon/tools/environment.sh
