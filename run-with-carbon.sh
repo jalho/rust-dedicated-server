74e2a2743c38d943e85127f5da9b05716261ef49
jalho <alho.jere@gmail.com>
2024-04-18 14:09:54 +0300

    Fix typo

--- run-with-carbon.sh
+++ run-with-carbon.sh
@@ -8,12 +8,12 @@ cd /home/rust/
 json_payload='{"content":"Going to start the server... First checking for updates!"}'
 curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$webhook_url"
 /home/rust/steamcmd/steamcmd.sh +force_install_dir /home/rust/ +login anonymous +app_update 258550 validate +quit
-json_payload='{"content":"Update check succesfull!"}'
+json_payload='{"content":"Update check successful!"}'
 curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$webhook_url"
 
 source /home/rust/carbon/tools/environment.sh
 
-json_payload='{"content":"Starting server... This usually takes around 20-30 minutes..."}'
+json_payload='{"content":"Starting server... This usually takes around 15 minutes..."}'
 curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$webhook_url"
 /home/rust/RustDedicated -batchmode +server.identity instance0 +rcon.port 28016 +rcon.web 1 +rcon.password Your_Rcon_Password
 
