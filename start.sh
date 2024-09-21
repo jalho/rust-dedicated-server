77f31eb56ed057da2bd2c85a731a4429a3c4e8ca
jalho <alho.jere@gmail.com>
2024-09-21 20:18:31 +0300

    Run game server on Debian 12.7

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@
 
 STEAMCMD_IMAGE="steamcmd/steamcmd:debian-12@sha256:4f82cc9dc0a7beadbbc7dc289811ead83a93e22c249c07509418bb7151ec361f"
 STEAMCMD_VOL_MANAGED="steamcmd"
+GAMESERVER_IMAGE="debian:12.7@sha256:b8084b1a576c5504a031936e1132574f4ce1d6cc7130bbcc25a28f074539ae6b"
 
 echo -n "INFO: Checking if Docker managed volume '$STEAMCMD_VOL_MANAGED' exists... "
 docker volume inspect steamcmd 2>/dev/null 1>/dev/null
@@ -35,3 +36,9 @@ docker run --rm \
  +force_install_dir /docker-managed-steamcmd-vol +login anonymous +app_update 258550 validate +quit
 set +e
 echo "Done!"
+
+docker run --rm \
+  -v $STEAMCMD_VOL_MANAGED:/steamcmd-installations \
+  -e LD_LIBRARY_PATH="/steamcmd-installations/RustDedicated_Data/Plugins/x86_64" \
+  $GAMESERVER_IMAGE \
+  /bin/sh -c "cd /steamcmd-installations && ./RustDedicated -batchmode +server.identity instance0 +rcon.port 28016 +rcon.web 1 +rcon.password instance0 +server.worldsize 1000"
